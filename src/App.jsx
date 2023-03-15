24ca63ce370714eb1d374954f874df780191aff6
Boluwatife <omotayodaniel10@gmail.com>
2023-03-15 14:09:09 +0100

    feat: react-router installed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1 className="text-sm font-bold">Artsy</h1>
+      <h1 className="text-3xl font-bold">Artsy</h1>
     </div>
   );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontSize: {
+      "3xl": ["64px", "156.7px"],
+      "2xl": ["48px", "118px"],
+      xl: ["32px", "90px"],
+      lg: ["24px", "70px"],
+      md: ["20px", "50.5px"],
+      sm: ["18px", "39.5px"],
+      base: "14px",
+    },
     extend: {},
   },
   plugins: [],
