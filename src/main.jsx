f8efe6b12fc7e6546ffb783d2951aa253188de80
omotayobolu <omotayodaniel10@gmail.com>
2025-03-23 15:03:33 +0100

    feat: react query import

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import {
   Navigate,
   RouterProvider,
 } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { Provider } from "react-redux";
 import store from "./store/index";
@@ -68,10 +69,14 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>
 );
