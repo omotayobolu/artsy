0f4397b084a69510b043e6ad0fe727b14efbae25
omotayobolu <omotayodaniel10@gmail.com>
2025-03-25 19:46:59 +0100

    feat: change to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,44 @@
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
 import Creators from "./Creators";
 import FeaturedProduct from "./FeaturedProduct";
 import HomeFooter from "./HomeFooter";
 import Hero from "./Hero";
 import UpcomingAuctions from "./UpcomingAuctions";
+import { getMarketplaceData, getAuctionsData } from "../../apis/artsy-api";
 
 const Home = () => {
+  const {
+    data: marketplace,
+    isLoading: marketplaceLoading,
+    error: marketplaceError,
+  } = useQuery({
+    queryKey: ["marketplace"],
+    queryFn: getMarketplaceData,
+  });
+
+  const {
+    data: auctions,
+    isLoading: auctionsLoading,
+    error: auctionsError,
+  } = useQuery({
+    queryKey: ["auctions"],
+    queryFn: getAuctionsData,
+  });
+
+  if (marketplaceLoading || auctionsLoading) {
+    return <div className="text-center">Loading...</div>;
+  }
+
+  if (marketplaceError || auctionsError) {
+    return <div>{marketplaceError?.message || auctionsError?.message}</div>;
+  }
+
   return (
     <section id="home">
       <Hero />
-      <FeaturedProduct />
-      <UpcomingAuctions />
+      <FeaturedProduct marketplace={marketplace} />
+      <UpcomingAuctions auctions={auctions.slice(0, 4)} />
       <Creators />
       <HomeFooter />
     </section>
