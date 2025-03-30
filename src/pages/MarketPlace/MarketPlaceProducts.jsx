5ad402bcea9b57207f93c41d290c162575f845a6
omotayobolu <omotayodaniel10@gmail.com>
2025-03-30 14:52:02 +0100

    refactor: new api data and functionalities

--- src/pages/MarketPlace/MarketPlaceProducts.jsx
+++ src/pages/MarketPlace/MarketPlaceProducts.jsx
@@ -1,46 +1,96 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, Link } from "react-router-dom";
-import { MarketPlaceData } from "../../data/MarketPlaceData";
-import Price from "../../assets/images/price.png";
 import PrimaryBtn from "../../components/PrimaryBtn";
 import { BsArrowRight } from "react-icons/bs";
-import { CiHeart } from "react-icons/ci";
+import { IoIosHeartEmpty, IoMdHeart } from "react-icons/io";
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 import { cartActions } from "../../store/cart-slice";
+import { useQuery } from "@tanstack/react-query";
+import { getMarketplaceProduct } from "../../utils/artsy-api";
+import { formatPrice } from "../../utils/priceFormatter";
+import axiosInstance, { getToken } from "../../utils/axiosInstance";
+import { toast, Toaster } from "sonner";
 
 const MarketPlaceProducts = () => {
-  const dispatch = useDispatch();
-  const cart = useSelector((state) => state.cart);
+  // const dispatch = useDispatch();
+  // const cart = useSelector((state) => state.cart);
+  // dispatch(
+  //   cartActions.addItemToCart({
+  //     id: product.id,
+  //     name: product.name,
+  //     actualPrice: product.price,
+  //     price: product.price,
+  //     creator: product.creator,
+  //     size: product.size,
+  //     image: product.image,
+  //     quantity: number,
+  //     totalProducts: cart.totalProducts,
+  //     totalPrice: cart.totalPrice,
+  //   })
+  // );
 
-  const addProductToCartHandler = () => {
-    dispatch(
-      cartActions.addItemToCart({
-        id: product.id,
+  const addProductToCartHandler = async () => {
+    try {
+      const response = await axiosInstance.post("/cart", {
+        userId: getToken(),
+        productId: product._id,
         name: product.name,
-        actualPrice: product.price,
         price: product.price,
-        creator: product.creator,
-        size: product.size,
-        image: product.image,
         quantity: number,
-        totalProducts: cart.totalProducts,
-        totalPrice: cart.totalPrice,
-      })
-    );
+        category: product.category,
+        creator: product.creator,
+        location: product.location,
+        image: product.image,
+      });
+      toast.success("Product added to cart successfully!");
+    } catch (error) {
+      console.error("Error adding product to cart:", error);
+      toast.error(error.response.data.message);
+    }
   };
 
   const { id } = useParams();
-  const product = MarketPlaceData.find((item) => item.id === Number(id));
+  const [isLiked, setIsLiked] = useState(false);
+  const [descriptionActive, setDescriptionActive] = useState(false);
+  const [listingActive, setListingActive] = useState(false);
+  const [statusActive, setStatusActive] = useState(false);
 
-  // to format views
-  function formatViews(views) {
+  const {
+    data: product,
+    isLoading: productLoading,
+    isError: productError,
+  } = useQuery({
+    queryKey: ["product", id],
+    queryFn: () => getMarketplaceProduct(id),
+  });
+
+  const handleLikeStatus = () => {
+    try {
+      axiosInstance
+        .patch(`/marketplace/${id}`, {
+          isLiked: !isLiked,
+        })
+        .then((res) => {
+          console.log(res.data);
+          setIsLiked(res.data.isLiked);
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (product) {
+      setIsLiked(product.isLiked);
+    }
+  }, [product]);
+
+  const formatViews = (views) => {
     if (views >= 1000) {
       return (views / 1000).toFixed(1) + "K";
     }
-  }
-  const views = product.views;
-  const formattedViews = formatViews(views);
+  };
 
   const [number, setNumber] = useState(1);
 
@@ -55,12 +105,12 @@ const MarketPlaceProducts = () => {
     }
   };
 
-  const [descriptionActive, setDescriptionActive] = useState(false);
-  const [listingActive, setListingActive] = useState(false);
-  const [statusActive, setStatusActive] = useState(false);
+  if (productLoading) return <p className="text-center">Loading...</p>;
+  if (productError) return <p className="text-center">Error...</p>;
 
   return (
     <div className="lg:my-[2%] lg:mx-[8%] sm:mx-[5%] mx-[3%]">
+      <Toaster position="top-right" richColors />
       <p className="text-[#BCB7B7] my-4">
         Home/ Marketplace/ Editorials/{" "}
         <span className="text-pure-black">{product.name}</span>
@@ -71,18 +121,15 @@ const MarketPlaceProducts = () => {
             <img
               src={product.image}
               alt={product.name}
-              className="lg:w-[525px] lg:h-[600px] max-w-full"
+              className="max-h-full h-[600px] w-full"
             />
           </div>
           <div className="lg:w-[50%] w-full">
-            <div className="w-full lg:py-4 py-2 lg:px-8 px-6 flex flex-row items-center justify-between">
-              <h3 className="lg:text-[40px] text-secondary-black">
+            <div className="w-full lg:py-4 py-2 lg:px-8 px-6 flex flex-row items-center justify-between gap-8">
+              <h3 className="lg:text-[40px] leading-normal text-secondary-black font-satoshi">
                 {product.name}
               </h3>
-              <div className="flex flex-row items-center">
-                <img src={Price} alt="" />
-                <h3 className="font-medium">{"0." + product.price}</h3>
-              </div>
+              <h3 className="font-medium">{formatPrice(product.price)}</h3>
             </div>
             <div className="border-y border-secondary-black lg:py-4 py-2 lg:px-8 px-6">
               <p className="my-2 text-[#616161]">
@@ -95,7 +142,7 @@ const MarketPlaceProducts = () => {
               <p className="my-2 font-medium">
                 Total views:{" "}
                 <span className="text-secondary-black">
-                  {formattedViews} views
+                  {formatViews(product.views)} views
                 </span>
               </p>
               <div className="text-[30px] my-2 flex flex-row items-center gap-4 text-secondary-black">
@@ -122,9 +169,22 @@ const MarketPlaceProducts = () => {
                 >
                   Add to cart <BsArrowRight color="#F5F4F4" />
                 </PrimaryBtn>
-                <div className="border border-primary-black py-2 px-3">
-                  <CiHeart fontSize="35px" cursor="pointer" />
-                </div>
+                <button className="border border-primary-black py-2 px-3">
+                  {isLiked ? (
+                    <IoMdHeart
+                      fontSize="35px"
+                      cursor="pointer"
+                      color="red"
+                      onClick={handleLikeStatus}
+                    />
+                  ) : (
+                    <IoIosHeartEmpty
+                      fontSize="35px"
+                      cursor="pointer"
+                      onClick={handleLikeStatus}
+                    />
+                  )}
+                </button>
               </div>
             </div>
             <div className="flex flex-col">
@@ -157,13 +217,9 @@ const MarketPlaceProducts = () => {
                       <MdKeyboardArrowDown fontSize="30px" />
                     )}
                   </div>
-                  <Link>
-                    {listingActive && (
-                      <p className="text-sm pt-4 hover:text-orange">
-                        {product.listing}
-                      </p>
-                    )}
-                  </Link>
+                  {listingActive && (
+                    <p className="text-sm pt-4">{product.listing}</p>
+                  )}
                 </div>
               </div>
             </div>
