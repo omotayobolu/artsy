3bf6136365c7389aff8e8657c230927e856f174e
omotayobolu <omotayodaniel10@gmail.com>
2025-04-08 12:16:29 +0100

    refactor: removed tab

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -15,42 +15,6 @@ const Cart = () => {
 
   return (
     <section className="lg:my-[1%] lg:mx-[8%] sm:mx-[5%] mx-[3%]">
-      <div className="grid place-items-center">
-        {cart.cart.products.length > 0 && (
-          <ul className="md:flex hidden flex-row items-center justify-center gap-8 border-b border-grey4 border-opacity-30">
-            <NavLink
-              to="shopping-cart"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-primary-black border-b border-secondary-black cart-link"
-                  : "cart-link"
-              }
-            >
-              Shopping Cart
-            </NavLink>
-            <NavLink
-              to="shipping-details"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-primary-black border-b border-secondary-black cart-link"
-                  : "cart-link"
-              }
-            >
-              Shipping details
-            </NavLink>
-            <NavLink
-              to="payment-details"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-primary-black border-b border-secondary-black cart-link"
-                  : "cart-link"
-              }
-            >
-              Payment details
-            </NavLink>
-          </ul>
-        )}
-      </div>
       <div>
         <Outlet />
       </div>
