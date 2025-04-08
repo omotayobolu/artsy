d5f76538141f534d3a224776c6a7be32aea6ce5b
omotayobolu <omotayodaniel10@gmail.com>
2025-04-08 12:12:43 +0100

    feat: get cart data using react-query

--- src/pages/Cart/CartTotals.jsx
+++ src/pages/Cart/CartTotals.jsx
@@ -1,27 +1,34 @@
 import React from "react";
 import CartSummary from "../../components/CartSummary";
 import { formatPrice } from "../../utils/priceFormatter";
+import { useQuery } from "@tanstack/react-query";
+import { getCart } from "../../utils/artsy-api";
 
-const CartTotals = ({ cart }) => {
+const CartTotals = () => {
+  const { data: cart } = useQuery({
+    queryKey: ["cart"],
+    queryFn: getCart,
+  });
+
   return (
     <div className="flex flex-col gap-8 font-satoshi">
       <CartSummary>
         <h4 className="text-grey3 font-normal">Products in cart:</h4>
         <span className="text-primary-black text-lg font-normal">
-          {cart.totalItems}
-          {cart.totalItems > 1 ? " items" : " item"}
+          {cart.cart.totalItems}
+          {cart.cart.totalItems > 1 ? " items" : " item"}
         </span>
       </CartSummary>
       <CartSummary>
         <h4 className="text-grey3 font-normal">Shipping:</h4>
         <span className="text-primary-black text-lg font-normal">
-          {formatPrice(cart.shipping)}
+          {formatPrice(cart.cart.shipping)}
         </span>
       </CartSummary>
       <CartSummary>
         <h4 className="text-grey3 font-normal">Total:</h4>
         <span className="text-primary-black text-lg font-normal">
-          {formatPrice(cart.roundedTotal)}
+          {formatPrice(cart.cart.roundedTotal)}
         </span>
       </CartSummary>
     </div>
