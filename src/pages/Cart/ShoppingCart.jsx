4dd5e99c091c12f049f07ae041696eaf1ceece14
omotayobolu <omotayodaniel10@gmail.com>
2025-04-08 12:32:05 +0100

    feat: handled check out, integraated withstripe

--- src/pages/Cart/ShoppingCart.jsx
+++ src/pages/Cart/ShoppingCart.jsx
@@ -6,6 +6,7 @@ import CartTotals from "./CartTotals";
 import SecondaryBtn from "../../components/SecondaryBtn";
 import { useQuery } from "@tanstack/react-query";
 import { getCart } from "../../utils/artsy-api";
+import axiosInstance, { getToken } from "../../utils/axiosInstance";
 
 const ShoppingCart = () => {
   const {
@@ -16,6 +17,25 @@ const ShoppingCart = () => {
     queryKey: ["cart"],
     queryFn: getCart,
   });
+
+  const handleCheckout = async () => {
+    try {
+      const response = await axiosInstance.post("/checkout", {
+        products: cart.cart.products,
+        userId: getToken(),
+      });
+
+      console.log(response);
+
+      if (response.data.url) {
+        window.location.assign(response.data.url);
+      } else {
+        console.error("No URL returned from the server");
+      }
+    } catch (error) {
+      console.error("Error during checkout:", error);
+    }
+  };
 
   if (cartLoading) {
     <p className="text-center">Loading...</p>;
@@ -32,13 +52,16 @@ const ShoppingCart = () => {
           <CartItems cart={cart.cart.products} />
           <hr style={{ color: "#747474", height: "0.3px", opacity: "0.3" }} />
           <div className="mt-16 flex lg:flex-row flex-col-reverse items-center gap-24 w-full">
-            <div className="lg:w-[40%] w-full grid gap-4 place-items-center">
-              <PrimaryBtn className="text-md w-full py-4">
-                <Link to="/cart/shipping-details">Proceed to checkout</Link>
+            <div className="lg:w-[40%] w-full flex flex-col justify-center gap-10">
+              <PrimaryBtn
+                className="text-md w-full py-4"
+                onClick={handleCheckout}
+              >
+                Proceed to checkout
               </PrimaryBtn>
-              <Link to="/marketplace">
-                <SecondaryBtn>Continue Shopping</SecondaryBtn>
-              </Link>
+              <SecondaryBtn>
+                <Link to="/marketplace">Continue Shopping</Link>
+              </SecondaryBtn>
             </div>
             <div className="lg:w-[60%] w-full">
               <CartTotals cart={cart.cart} />
