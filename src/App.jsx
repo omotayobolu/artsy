899bd1d2dbfd35b11db3ad6fbb313cf604b8f425
omotayobolu <omotayodaniel10@gmail.com>
2025-04-08 12:41:10 +0100

    refactor: cleared redux tools

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
 import Navbar from "./components/Navbar";
-import { fetchCartData, sendCartData } from "./store/cart-actions";
 
 const App = () => {
   return (
